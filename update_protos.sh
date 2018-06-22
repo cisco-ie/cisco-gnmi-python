cf390108183925ce8b204625410934fb5a5fb649
Remington Campbell <remcampb@cisco.com>
2018-06-22 12:39:14 -0700

    Add submodule update before compilation

--- update_protos.sh
+++ update_protos.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+git submodule update --remote
 protoc --proto_path=. --python_out=gnmi-python/proto github.com/openconfig/gnmi/proto/gnmi/gnmi.proto
 cp gnmi-python/proto/github/com/openconfig/gnmi/proto/gnmi/*.py gnmi-python/proto/
 rm -rf gnmi-python/proto/github
