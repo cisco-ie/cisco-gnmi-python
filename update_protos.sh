16d5b9da4d59ba4a4ca92c7bc34210e0026f6c67
Remington Campbell <remcampb@cisco.com>
2018-06-22 14:46:19 -0700

    Generate gNMI gRPC stub

--- update_protos.sh
+++ update_protos.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+echo "Updating proto sources..."
 git submodule update --remote
-protoc --proto_path=. --python_out=gnmi-python/proto github.com/openconfig/gnmi/proto/gnmi/gnmi.proto
+echo "Compiling protos..."
+python -m grpc_tools.protoc --proto_path=. --python_out=gnmi-python/proto --grpc_python_out=gnmi-python/proto github.com/openconfig/gnmi/proto/gnmi/gnmi.proto
+echo "Copying compiled protos..."
 cp gnmi-python/proto/github/com/openconfig/gnmi/proto/gnmi/*.py gnmi-python/proto/
+echo "Copying compiled gRPC stubs..."
+cp gnmi-python/proto/github.com/openconfig/gnmi/proto/gnmi/*.py gnmi-python/proto/
+echo "Cleaning up..."
 rm -rf gnmi-python/proto/github
+rm -rf gnmi-python/proto/github.com
