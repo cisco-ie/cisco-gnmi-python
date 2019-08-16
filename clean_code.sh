bd3a31b50511800e6d6632948953aed4cd5af33e
Remington Campbell <remcampb@cisco.com>
2019-08-16 15:40:00 -0700

    Disable some pylint related to black formatting

--- clean_code.sh
+++ clean_code.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 pipenv run black --safe --verbose --exclude proto gnmi
-pipenv run pylint gnmi/*.py
+# Many disabled to accomodate black disagreements...
+# Many failures due to protos being runtime-functional and difficult to lint.
+pipenv run pylint --disable line-too-long --disable pointless-string-statement --disable no-member --disable wrong-import-position --disable wrong-import-position --disable bad-continuation gnmi/*.py
