1583c818f836c1b7d30c4ea4d57b2140f0702509
Remington Campbell <remcampb@cisco.com>
2019-09-04 14:40:21 -0700

    Rename from gnmi to cisco_gnmi

--- update_protos.sh
+++ update_protos.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 echo "Updating proto sources..."
 git submodule update --remote
-cp github.com/openconfig/gnmi/proto/gnmi/*.proto gnmi/proto/
-cp github.com/openconfig/gnmi/proto/gnmi_ext/*.proto gnmi/proto/
+cp github.com/openconfig/gnmi/proto/gnmi/*.proto cisco_gnmi/proto/
+cp github.com/openconfig/gnmi/proto/gnmi_ext/*.proto cisco_gnmi/proto/
 echo "Fixing proto imports..."
 python -c "
-with open('gnmi/proto/gnmi.proto', 'r') as gnmi_fd:
+with open('cisco_gnmi/proto/gnmi.proto', 'r') as gnmi_fd:
   file_content = gnmi_fd.read()
 file_content = file_content.replace('github.com/openconfig/gnmi/proto/gnmi_ext/', '', 1)
-with open('gnmi/proto/gnmi.proto', 'w') as gnmi_fd:
+with open('cisco_gnmi/proto/gnmi.proto', 'w') as gnmi_fd:
   gnmi_fd.write(file_content)
 "
 echo "Compiling protos..."
-python -m grpc_tools.protoc --proto_path=gnmi/proto --python_out=gnmi/proto --grpc_python_out=gnmi/proto gnmi.proto gnmi_ext.proto
+pipenv run python -m grpc_tools.protoc --proto_path=cisco_gnmi/proto --python_out=cisco_gnmi/proto --grpc_python_out=cisco_gnmi/proto gnmi.proto gnmi_ext.proto
 echo "Fixing compiled Python imports..."
 python -c "
-with open('gnmi/proto/gnmi_pb2_grpc.py', 'r') as gnmi_fd:
+with open('cisco_gnmi/proto/gnmi_pb2_grpc.py', 'r') as gnmi_fd:
   file_content = gnmi_fd.read()
 file_content = file_content.replace('import gnmi_pb2', 'from . import gnmi_pb2', 1)
-with open('gnmi/proto/gnmi_pb2_grpc.py', 'w') as gnmi_fd:
+with open('cisco_gnmi/proto/gnmi_pb2_grpc.py', 'w') as gnmi_fd:
   gnmi_fd.write(file_content)
-with open('gnmi/proto/gnmi_pb2.py', 'r') as gnmi_fd:
+with open('cisco_gnmi/proto/gnmi_pb2.py', 'r') as gnmi_fd:
   file_content = gnmi_fd.read()
 file_content = file_content.replace('import gnmi_ext_pb2', 'from . import gnmi_ext_pb2', 1)
-with open('gnmi/proto/gnmi_pb2.py', 'w') as gnmi_fd:
+with open('cisco_gnmi/proto/gnmi_pb2.py', 'w') as gnmi_fd:
   gnmi_fd.write(file_content)
 "
 echo "Cleaning up..."
-rm gnmi/proto/*.proto
+rm cisco_gnmi/proto/*.proto
