aaf34017482e0d1b1fa8c3c6e1a737dedc2f7a17
Remington Campbell <remcampb@cisco.com>
2019-09-04 17:11:23 -0700

    Add clarifying comment

--- hygiene.sh
+++ hygiene.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+# Script which autoformats (black) and lints (pylint) code
 pipenv run black --safe --verbose --exclude proto src/cisco_gnmi
 # Many disabled to accomodate black disagreements...
 # Many failures due to protos being runtime-functional and difficult to lint.
-pipenv run pylint --disable line-too-long --disable pointless-string-statement --disable no-member --disable wrong-import-position --disable wrong-import-position --disable bad-continuation src/cisco_gnmi/*.py
+pipenv run pylint --disable line-too-long --disable pointless-string-statement --disable no-member --disable wrong-import-position --disable bad-continuation src/cisco_gnmi/*.py
