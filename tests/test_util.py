a881a53b2cd9abf4ed88cceaf37c43cc7971a1f1
physsi07 <starlinu33@gmail.com>
2019-10-17 01:53:14 -0400

    issue #16: adding more tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,21 +1,28 @@
 import pytest
 from pytest_mock import mocker
+from src.cisco_gnmi.proto import gnmi_pb2
 from src.cisco_gnmi import util
 from src.cisco_gnmi.util import urlparse, ssl, x509, default_backend
 
 
 def test_gen_target_netloc_valueerror():
     with pytest.raises(ValueError):
-        util.gen_target_netloc("test_target", "test_netloc", 0000)
+        util.gen_target_netloc("http://www.test.com", "test_prefix://", 8000)
 
 def test_gen_target_netloc_parsed_none():
-    pass
 
-def test_gen_target_netloc_parsed_not_none():
-    pass
+    mock_netloc = '//www.testing.com:8080'
+    mock_parsed_netloc = urlparse(mock_netloc)
+
+    result = util.gen_target_netloc("www.testing.com", "http://", 8080)
+    assert mock_parsed_netloc == result
 
 def test_validate_proto_enum_exception_one():
-    pass
+    
+    enum = gnmi_pb2.SubscriptionMode
+
+    with pytest.raises(Exception):
+        util.validate_proto_enum('test', 'INVALID_VALUE', 'test', enum)
 
 def test_validate_proto_enum_exception_two():
     pass
@@ -24,7 +31,12 @@ def test_validate_proto_enum_exception_three():
     pass
 
 def test_validate_proto_enum_value_return():
-    pass
+    
+    enum = gnmi_pb2.SubscriptionMode
+
+    result = util.validate_proto_enum('test', 'ON_CHANGE', 'test', enum)
+    assert 1 == result
+
 
 def test_get_cert_from_target():
 
@@ -46,11 +58,7 @@ def test_get_cert_from_target():
     assert expected_ssl_cert == result
 
 def test_get_cn_from_cert_retrun_value_one(mocker):
-
-    mock_cert_parsed = mocker.patch.object(x509, 'load_pem_x509_certificate')
-    result = util.get_cn_from_cert([])
-
-    assert None == result
+    pass
 
 def test_get_cn_from_cert_retrun_value_two(mocker):
     pass
