b0ea34ce5186a7b0c449d82924e6d2a755d98379
miott <miott@cisco.com>
2020-04-27 18:59:52 -0700

    Fix for check_config and prefix.

--- src/cisco_gnmi/client.py
+++ src/cisco_gnmi/client.py
@@ -31,6 +31,9 @@ from . import proto
 from . import util
 
 
+logger = logging.getLogger(__name__)
+
+
 class Client(object):
     """gNMI gRPC wrapper client to ease usage of gNMI.
 
@@ -163,6 +166,9 @@ class Client(object):
             request.use_models = use_models
         if extension:
             request.extension = extension
+
+        logger.debug(str(request))
+
         get_response = self.service.Get(request)
         return get_response
 
@@ -190,7 +196,7 @@ class Client(object):
         """
         request = proto.gnmi_pb2.SetRequest()
         if prefix:
-            request.prefix = prefix
+            request.prefix.CopyFrom(prefix)
         test_list = [updates, replaces, deletes]
         if not any(test_list):
             raise Exception("At least update, replace, or delete must be specified!")
@@ -207,6 +213,9 @@ class Client(object):
             request.delete.extend(deletes)
         if extensions:
             request.extension.extend(extensions)
+
+        logger.debug(str(request))
+
         response = self.service.Set(request)
         return response
 
@@ -244,6 +253,9 @@ class Client(object):
                 )
             if extensions:
                 subscribe_request.extensions.extend(extensions)
+
+            logger.debug(str(subscribe_request))
+
             return subscribe_request
 
         response_stream = self.service.Subscribe(
