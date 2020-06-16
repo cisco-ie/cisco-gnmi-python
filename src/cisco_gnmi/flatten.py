bbe83dadff3aa6732b87925028be5d7444b09877
Remington Campbell <remcampb@cisco.com>
2020-06-16 16:22:42 -0700

    JSON flattening functions

--- src/cisco_gnmi/flatten.py
+++ src/cisco_gnmi/flatten.py
@@ -32,32 +32,43 @@ def _flatten_subscribe_response(subscribe_response):
     pass
 
 def _flatten_yang_json(yang_json):
+    return __flatten_yang_json_element("", yang_json)
+
+def __flatten_yang_json_element(prefix, fields, convert_strings=True):
     flattened = {}
-    for key, value in yang_json:
-        ykey = "/{}".format(key)
-        path, value = __flatten_yang_json_element(ykey, value)
-        flattened[path] = value
-    return flattened
-
-def __flatten_yang_json_element(prefix, fields):
-    flattened = []
-    for key, value in fields.items():
-        curr_path = "{}/{}".format(prefix, key)
-        keys = {}
-        values = {}
-        if isinstance(value, dict):
-            for _key, _value in value.items():
-                if isinstance(_value, str):
+    keys = {}
+    values = {}
+    to_traverse = {}
+    if isinstance(fields, list):
+        for elem in fields:
+            flattened.update(__flatten_yang_json_element(prefix, elem))
+    elif isinstance(fields, dict):
+        for key, value in fields.items():
+            if isinstance(value, str):
+                if convert_strings:
                     try:
-                        values[_key] = int(_value)
+                        values[key] = int(value)
                     except ValueError:
-                        keys[_key] = _value
-                elif isinstance(_value, int):
-                    values[_key] = _value
-            
-
-
-            return __flatten_yang_json_element(curr_path, value)
-        if isinstance(value, list):
-            keys, fields = __flatten_yang_json_element(curr_path, value)
+                        try:
+                            values[key] = float(value)
+                        except ValueError:
+                            keys[key] = value
+                else:
+                    keys[key] = value
+            elif isinstance(value, (int, float)):
+                values[key] = value
+            elif isinstance(value, (dict, list)):
+                to_traverse[key] = value
+            else:
+                raise Exception("Unhandled element type!")
+        key_string = ""
+        for key, value in keys.items():
+            key_string += "[{key}={value}]".format(key=key, value=value)
+        for key, value in to_traverse.items():
+            keyed_elem = "{prefix}{keys}/{elem}".format(prefix=prefix, elem=key, keys=key_string)
+            flattened.update(__flatten_yang_json_element(keyed_elem, value, convert_strings))
+        for key, value in values.items():
+            keyed_elem = "{prefix}{keys}/{elem}".format(prefix=prefix, elem=key, keys=key_string)
+            flattened[keyed_elem] = value
+    return flattened
         
