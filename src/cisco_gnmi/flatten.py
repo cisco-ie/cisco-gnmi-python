56a444e876589e7fbf1c5a607d26fb4dec0d1e0d
Remington Campbell <remcampb@cisco.com>
2020-06-17 10:22:52 -0700

    Flatten protobufs and JSON values

--- src/cisco_gnmi/flatten.py
+++ src/cisco_gnmi/flatten.py
@@ -1,3 +1,4 @@
+import json
 from . import proto
 
 def parse_path_to_xpath(path):
@@ -10,40 +11,53 @@ def parse_path_to_xpath(path):
     return origin, xpath
 
 def flatten(message):
-    op_map = {
-        proto.gnmi_pb2.GetResponse: _flatten_get_response,
-        proto.gnmi_pb2.SubscribeResponse: _flatten_subscribe_response
+    flatten_method_map = {
+        proto.gnmi_pb2.GetResponse: _flatten_get_response
     }
     identified = False
-    for message_class, op in op_map.items():
+    flattened_message = None
+    for message_class, flatten_method in flatten_method_map.items():
         if isinstance(message, message_class):
             identified = True
-            op(message)
+            flattened_message = flatten_method(message)
     if not identified:
         raise Exception("Flatten not yet supported for message class!")
+    return flattened_message
 
 def _flatten_get_response(get_response):
     flattened_response = {}
     for notification in get_response.notification:
-        _, xpath = parse_path_to_xpath(notification.path)
-        value = 
+        _, xpath_prefix = parse_path_to_xpath(notification.prefix)
+        for update in notification.update:
+            _, xpath_update = parse_path_to_xpath(update.path)
+            xpath = ""
+            if xpath_prefix and xpath_update:
+                xpath = "{}/{}".format(xpath_prefix, xpath_update)
+            elif xpath_prefix:
+                xpath = xpath_prefix
+            elif xpath_update:
+                xpath = xpath_update
+            update_name = update.WhichOneOf("value")
+            update_value = getattr(update, update_name)
+            if update_name in {"json_val", "json_ietf_val"}:
+                raw_json = getattr(update, update_name)
+                serialized_json = json.load(raw_json)
+                sub_flattened_response = flatten_yang_json(xpath, serialized_json)
+                flattened_response.update(sub_flattened_response)
+            else:
+                flattened_response[xpath] = update_value
+    return flattened_response
 
-def _flatten_subscribe_response(subscribe_response):
-    pass
-
-def _flatten_yang_json(yang_json):
-    return __flatten_yang_json_element("", yang_json)
-
-def __flatten_yang_json_element(prefix, fields, convert_strings=True):
+def flatten_yang_json(prefix, yang_json, convert_strings=True):
     flattened = {}
     keys = {}
     values = {}
     to_traverse = {}
-    if isinstance(fields, list):
-        for elem in fields:
-            flattened.update(__flatten_yang_json_element(prefix, elem))
-    elif isinstance(fields, dict):
-        for key, value in fields.items():
+    if isinstance(yang_json, list):
+        for elem in yang_json:
+            flattened.update(flatten_yang_json(prefix, elem))
+    elif isinstance(yang_json, dict):
+        for key, value in yang_json.items():
             if isinstance(value, str):
                 if convert_strings:
                     try:
@@ -66,7 +80,7 @@ def __flatten_yang_json_element(prefix, fields, convert_strings=True):
             key_string += "[{key}={value}]".format(key=key, value=value)
         for key, value in to_traverse.items():
             keyed_elem = "{prefix}{keys}/{elem}".format(prefix=prefix, elem=key, keys=key_string)
-            flattened.update(__flatten_yang_json_element(keyed_elem, value, convert_strings))
+            flattened.update(flatten_yang_json(keyed_elem, value, convert_strings))
         for key, value in values.items():
             keyed_elem = "{prefix}{keys}/{elem}".format(prefix=prefix, elem=key, keys=key_string)
             flattened[keyed_elem] = value
